ed6600ef206f11551f860ed9247fd61af0a8d3b0
Felix <folivares@hawk.iit.edu>
2020-03-04 16:46:42 -0600

    commit 1

--- app/src/main/java/com/example/quotes/QuoteReaderActivity.java
+++ app/src/main/java/com/example/quotes/QuoteReaderActivity.java
@@ -3,10 +3,12 @@ package com.example.quotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -81,6 +83,18 @@ public class QuoteReaderActivity extends AppCompatActivity {
 
         ListView mListView = findViewById(R.id.quotes_list);
         mListView.setAdapter(new QuoteAdapter(this));
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int position,
+                                    long arg3) {
+                Intent i = new Intent(QuoteReaderActivity.this, QuoteDetail.class);
+                i.putExtra("position", position);
+                startActivity(i);
+            }
+        });
 
     }
 }
